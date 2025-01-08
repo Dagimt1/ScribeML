e5797d621f82c711cd955aea994dfa1cd4891f3d
Dagim Jalleta <dagimteriessa@gmail.com>
2025-01-07 22:42:30 -0600

    Updated App component with basic Tailwind CSS layout and UI improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='text-green-400'>Hello</h1>
+    <div className="flex flex-col p-4">
+      <section className="min-h-screen flex flex-col">
+        <header>
+          <h1>
+            Scribe
+            <span className="text-blue-400">ML</span>
+          </h1>
+        </header>
+        <main className="flex-1">
+        </main>
+      </section>
+      <h1 className="text-green-400">hello</h1>
+      <footer></footer>
     </div>
   )
 }
 
-export default App
+export default App;
