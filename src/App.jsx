15a2cdc464b4b94f71bc561070972567883ecdf3
Dagim Jalleta <dagimteriessa@gmail.com>
2025-01-07 22:28:07 -0600

    Initialize ScribeML project with Vite, React, and TailwindCSS setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-green-400'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
