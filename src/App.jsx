120015548a14a6a04c80604d8aab678a461fd2dd
Dagim Jalleta <dagimteriessa@gmail.com>
2025-01-09 21:27:59 -0600

    Add Information.jsx component with transcription and translation functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 import HomePage from './components/HomePage';
 import Header from './components/Header';
+import FileDisplay from './components/FileDisplay';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvailable = file || audioStream
 
   return (
-    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
-      <section className="min-h-screen flex flex-col">
-        <Header />
-        <HomePage />
-        
-      </section>
-    </div>
-  );
+      <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+          <section className='min-h-screen flex flex-col'>
+              <Header />
+              {isAudioAvailable ?(
+                <FileDisplay />
+
+              ) :(<HomePage setFile={setFile} setAudioStream={setAudioStream}/>)}
+          </section>
+          <h1 className='text-green-400'>hello</h1>
+          <footer></footer>
+      </div>
+  )
 }
 
-export default App;
+export default App
